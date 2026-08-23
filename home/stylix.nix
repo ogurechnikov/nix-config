edbef2759cfc2151dedba635065bf793f8a2386b
Sergey Ogurechnikov <ogurechnikov@gmail.com>
2026-08-24 00:31:39 +0700

    Convert alacritty to programs.alacritty, drop font family for stylix

--- home/stylix.nix
+++ home/stylix.nix
@@ -4,6 +4,14 @@
   stylix.polarity = "dark";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/default-dark.yaml";
 
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrainsMono Nerd Font Mono";
+    };
+    sizes.terminal = 11;
+  };
+
   stylix.targets = {
     alacritty.enable = true;
     neovim.enable = true;
