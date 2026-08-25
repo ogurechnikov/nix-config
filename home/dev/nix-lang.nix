ad94264218052a7226f0e5dff07c5e588f214665
Sergey Ogurechnikov <ogurechnikov@gmail.com>
2026-08-26 02:38:44 +0700

    Fix nixfmt package rename

--- home/dev/nix-lang.nix
+++ home/dev/nix-lang.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   home.packages = with pkgs; [
-    nil          # LSP-сервер для Nix
-    nixfmt-rfc-style   # форматтер (актуальное имя пакета для форматирования по RFC-стилю)
+    nil
+    nixfmt
   ];
 }
