1f21441d6b31b3a0c44f350d0b8941b2fbc09415
Sergey Ogurechnikov <ogurechnikov@gmail.com>
2026-08-26 15:40:12 +0700

    add qtdeclarative for qmlls, symlink tree-sitter grammar

--- home/dev/qml.nix
+++ home/dev/qml.nix
@@ -1,7 +1,13 @@
 { pkgs, inputs, ... }:
+let
+  qmljsGrammar = inputs.nix-qml-support.packages.${pkgs.system}.tree-sitter-qmljs;
+in
 {
   home.packages = [
-    inputs.nix-qml-support.packages.${pkgs.system}.tree-sitter-qmljs
+    qmljsGrammar
     inputs.nix-qml-support.packages.${pkgs.system}.qml-ts-mode
+    pkgs.qt6.qtdeclarative
   ];
+
+  home.file.".emacs.d/tree-sitter/libtree-sitter-qmljs.so".source = "${qmljsGrammar}/parser";
 }
